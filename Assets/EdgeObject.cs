d059514620feb5cc30836297a88366db23657edb
Andrew Erskine <erskine94@gmail.com>
2021-10-31 22:08:50 -0700

    nPoints straight line

--- Assets/EdgeObject.cs
+++ Assets/EdgeObject.cs
@@ -32,22 +32,33 @@ public class EdgeObject : MonoBehaviour
         lineRenderer.positionCount = nPoints;
         Vector3 p1 = points[0].position;
         Vector3 p2 = points[points.Length - 1].position;
+        Vector3 moveVec = (p2 - p1);
         float dx = p2.x - p1.x;
         float dy = p2.y - p1.y;
         float distance = Mathf.Sqrt(dx * dx + dy * dy);
         float a = Mathf.Atan2(dy, dx);
-        float cosa = Mathf.Cos(a);
-        float sina = Mathf.Sin(a);
+        // float cosa = Mathf.Cos(a);
+        // float sina = Mathf.Sin(a);
 
-        Vector3 pointPos = Vector3.zero;
+        // Vector3 pointPos = Vector3.zero;
+        // for (int i = 0; i < lineRenderer.positionCount; i++) {
+        //     // untransformed wave
+        //     float tx = (float)i / (float)nPoints;
+        //     float ty = amplitude * Mathf.Sin(tx * 2 * Mathf.PI) / wavelength;
+
+        //     // apply transform
+        //     float x = p1.x + distance * (tx * cosa - ty * sina);
+        //     float y = p1.y + distance * (tx * sina + ty * cosa);
+
+        //     lineRenderer.SetPosition(i, new Vector3(x, y, p1.z));
+        // }
+
         for (int i = 0; i < lineRenderer.positionCount; i++) {
-            // untransformed wave
-            float tx = (float)i / (float)nPoints;
-            float ty = amplitude * Mathf.Sin(tx * 2 * Mathf.PI / wavelength);
+            float tx = (float)i / (float)nPoints; // proportion along line
+            float ty = moveVec.y * tx;
 
-            // apply transform
-            float x = p1.x + distance * (tx * cosa - ty * sina);
-            float y = p1.y + distance * (tx * sina + ty * cosa);
+            float x = p1.x + (moveVec * tx).x;
+            float y = p1.y + (moveVec * tx).y;
 
             lineRenderer.SetPosition(i, new Vector3(x, y, p1.z));
         }
